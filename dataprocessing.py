f186711ed554769d839f44a8e992259ac39624f6
Nick Bossi <nickbossiwork@gmail.com>
2024-07-08 18:05:26 +0200

    Folder management

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,12 +1,13 @@
 import pandas as pd 
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 #Loading csv's
 
-apple_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\AAPL.csv"
-microsoft_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\MSFT.csv"
-file_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\stock_data.csv"
+apple_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\data\AAPL.csv"
+microsoft_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\data\MSFT.csv"
+file_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\data\stock_data.csv"
 
 appledf = pd.read_csv(apple_path)
 microdf = pd.read_csv(microsoft_path)
@@ -15,12 +16,16 @@ print(microdf.shape)
 
 #Truncating apple data rows to ensure same length as microsoft data
 appledf = appledf.tail(microdf.shape[0])
+print(appledf.tail())
+
 
 #Extracting only the opening price from the stocks
 applecolumn = appledf['Open'].values
 microcolumn = microdf['Open'].values
 
 file = pd.DataFrame(np.stack((applecolumn, microcolumn), axis=1), columns=['apple', 'microsoft'])
+plt.plot(file)
+plt.show()
 #file.to_csv(file_path, index=False)
 
 #creating a torch tensor of the stocks
--- stock_path_creation.py
+++ stock_path_creation.py
@@ -12,75 +12,77 @@ torch.cuda.empty_cache()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-depth = 10
-
-# File paths for training and testing sets
-train_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\train_stock_data.csv"
-test_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\test_stock_data.csv"
-
-# Gets training and testing data
-traindf = pd.read_csv(train_path)
-testdf = pd.read_csv(test_path)
-
-# Performs transpose and adds batch dimension for signature
-train_data = torch.unsqueeze(torch.tensor(traindf.values), 0)
-test_data = torch.unsqueeze(torch.tensor(testdf.values), 0)
-
-print(train_data.shape)
-
-# Initiating empty list which will hold all signatures
-train_signature_data = []
-test_signature_data = []
-
-# Getting dimensions of data to be looped over
-n = train_data.shape[1]
-print(n)
-m = test_data.shape[1]
-print(m)
-num_samples = int(n/depth)
-
-tempdf = train_data[:,0:depth,:]
-
-signature = signatory.signature(path = tempdf, depth = depth)
-
-inverse = invert_signature(signature=signature, depth=depth, channels = 2)[:,1:,]
-
-for i in range(num_samples):
-    temp_data = train_data[:,i*depth:(i+1)*depth,:]
-    temp_signature = signatory.signature(path = temp_data, depth = depth)
-    train_signature_data.append(temp_signature)
-
-for j in range(int(m/depth)):
-    temp_data = test_data[:,j*depth:(j+1)*depth,:]
-    temp_signature = signatory.signature(path = temp_data, depth = depth)
-    test_signature_data.append(temp_signature)
-
-print("finished")
-
-# Converting to tensors
-train_signature_data = torch.stack(train_signature_data, dim = 0).squeeze(1)
-test_signature_data = torch.stack(test_signature_data, dim = 0)
-
-#extracting mean and variances over columns
-column_mean = torch.mean(train_signature_data, dim = 0)
-column_std = torch.std(train_signature_data, dim = 0)
-
-#Unsqueezing again
-train_signature_data = ((train_signature_data- column_mean)/column_std).unsqueeze(1)
-print(train_signature_data.shape)
 
 def get_mean_std():
+    train_sig_data = torch.load('data/train_sig_data.pt')
+    column_mean = torch.mean(train_sig_data, dim = 0)
+    column_std = torch.std(train_sig_data, dim = 0)
     return column_mean, column_std
 
+def main():
 
-torch.save(train_signature_data, 'train_sig_data.pt')
-torch.save(test_signature_data, 'test_sig_data.pt')
-loaded_tensor = torch.load('train_sig_data.pt')
-print(loaded_tensor.shape)
-print(loaded_tensor[0,0,:])
+    depth = 10
 
+    # File paths for training and testing sets
+    train_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\data\train_stock_data.csv"
+    test_path = r"d:\Documents\UCT\Honours\Honours_Project\Code\Stocks\data\test_stock_data.csv"
 
+    # Gets training and testing data
+    traindf = pd.read_csv(train_path)
+    testdf = pd.read_csv(test_path)
 
-# for i in range(int(loaded_tensor.shape[2]/4)):
-#     print(loaded_tensor[0,0,i])
-#print(train_signature_data.shape)
+    # Performs transpose and adds batch dimension for signature
+    train_data = torch.unsqueeze(torch.tensor(traindf.values), 0)
+    test_data = torch.unsqueeze(torch.tensor(testdf.values), 0)
+
+    print(train_data.shape)
+
+    # Initiating empty list which will hold all signatures
+    train_signature_data = []
+    test_signature_data = []
+
+    # Getting dimensions of data to be looped over
+    n = train_data.shape[1]
+    print(n)
+    m = test_data.shape[1]
+    print(m)
+    num_samples = int(n/depth)
+
+    tempdf = train_data[:,0:depth,:]
+
+    signature = signatory.signature(path = tempdf, depth = depth)
+
+    inverse = invert_signature(signature=signature, depth=depth, channels = 2)[:,1:,]
+
+    for i in range(num_samples):
+        temp_data = train_data[:,i*depth:(i+1)*depth,:]
+        temp_signature = signatory.signature(path = temp_data, depth = depth)
+        train_signature_data.append(temp_signature)
+
+    for j in range(int(m/depth)):
+        temp_data = test_data[:,j*depth:(j+1)*depth,:]
+        temp_signature = signatory.signature(path = temp_data, depth = depth)
+        test_signature_data.append(temp_signature)
+
+    print("finished")
+
+    # Converting to tensors
+    train_signature_data = torch.stack(train_signature_data, dim = 0).squeeze(1)
+    test_signature_data = torch.stack(test_signature_data, dim = 0)
+
+    #extracting mean and variances over columns
+    column_mean = torch.mean(train_signature_data, dim = 0)
+    column_std = torch.std(train_signature_data, dim = 0)
+
+    #Unsqueezing again
+    train_signature_data = ((train_signature_data- column_mean)/column_std).unsqueeze(1)
+    print(train_signature_data.shape)
+
+    torch.save(train_signature_data, 'data/train_sig_data.pt')
+    torch.save(test_signature_data, 'data/test_sig_data.pt')
+    loaded_tensor = torch.load('data/train_sig_data.pt')
+    print(loaded_tensor.shape)
+    print(loaded_tensor[0,0,:])
+
+if __name__ == "__main__":
+    main()
